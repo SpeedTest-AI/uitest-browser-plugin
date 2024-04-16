305dc89fc931e91d7c4fd90aed6ed1b47a9f45ba
taoli <taoli@taolis-MacBook-Pro.local>
2024-04-16 18:04:08 +0800

    add plugin

--- content-scripts.js
+++ content-scripts.js
@@ -1,11 +1,18 @@
+let selectedContent = '';
+// Function to handle mouseup event
+function handleMouseUp(event) {
+        // Get selected text
+        selectedContent = window.getSelection().toString().trim();
+        console.log("---get selected content---" + selectedContent)
+}
+document.addEventListener('mouseup', handleMouseUp);
+
 chrome.runtime.onMessage.addListener(
         async function (message, sender, sendResponse) {
                 console.log(sender.tab ?
                         "from a content script:" + sender.tab.url :
-                        "from the extension");
-                //         if (message.action === "getPageHTML")
-                //                 sendResponse({ pageHTML: document.documentElement.outerHTML });
-                // }
-                if (message.action === "getPageHTML") { await chrome.runtime.sendMessage({ pageHTML: document.documentElement.outerHTML }) }
+                        "from the extension,message action: " + message.action);
+                if (message.action === "getWholeHTML") { await chrome.runtime.sendMessage({ type: "getWholeHTML", pageHTML: document.body.outerHTML }) }
+                else if (message.action === "getSelectedHTML") { await chrome.runtime.sendMessage({ type: "getSelectedHTML", pageHTML: selectedContent }) }
         }
 );
--- src/popup.js
+++ src/popup.js
@@ -2,17 +2,46 @@
 document.addEventListener("DOMContentLoaded", async function () {
         document.getElementById("generate").addEventListener("click", async () => {
                 const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-                const response = await chrome.tabs.sendMessage(tab.id, { action: "getPageHTML" });
-                console.log(response);
+                let range = document.getElementById("range");
+                if (range.value === "selectedPage") { const response = await chrome.tabs.sendMessage(tab.id, { action: "getSelectedHTML" }); }
+                else { const response = await chrome.tabs.sendMessage(tab.id, { action: "getWholeHTML" }) };
         });
+
+        document.getElementById("clear").addEventListener("click", function () {
+                document.getElementById("result").value = ""
+        })
+        document.getElementById("copy").addEventListener("click", function () {
+                let copiedText = document.getElementById("result");
+                copiedText.select();
+                copiedText.setSelectionRange(0, 99999); //For mobile devices
+                navigator.clipboard.writeText(copiedText.value);
+        })
 });
+
+function getMiddleRowContent(bodyContent, numRows) {
+        const lines = bodyContent.split('\n');
+        if (lines.length > numRows) {
+                const startIndex = Math.floor((lines.length - numRows) / 2);
+                const endIndex = startIndex + numRows;
+                const middleRows = lines.slice(startIndex, endIndex);
+                return middleRows.join('\n');
+        } else {
+                return bodyContent
+        }
+}
+
+// function geneareLocator() { }
 
 chrome.runtime.onMessage.addListener(
         function (request, sender, sendResponse) {
                 console.log(sender.tab ?
                         "from a content script:" + sender.tab.url :
                         "from the extension");
-                if (request.pageHTML)
-                        document.getElementById("result").value = request.pageHTML;
+                console.log(request.pageHTML)
+                if (request.type === "getWholeHTML") {
+                        const number = document.getElementById('number').value;
+                        document.getElementById("result").value = getMiddleRowContent(request.pageHTML, number);
+                }
+                else if (request.type === "getSelectedHTML") { document.getElementById("result").value = request.pageHTML; }
         }
 );
